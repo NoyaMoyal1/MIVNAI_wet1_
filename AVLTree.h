d45d6e2e56cd1a7a29635019c88bbf54f3799529
sapirfishman <sapirfishman@campus.technion.ac.il>
2024-02-20 23:34:36 +0200

    push first time

---
+++ AVLTree.h
@@ -0,0 +1,194 @@
+//
+// Created by Sapir on 20/02/2024.
+//
+
+#ifndef MIVNAI_WET1_NEW_AVLTREE_H
+#define MIVNAI_WET1_NEW_AVLTREE_H
+#include "Node.h"
+
+template <typename K, typename D>
+class AVLTree {
+private:
+    Node<K,D>* m_root;
+
+public:
+    //constructor
+    AVLTree(K rootKey): m_root(new Node<K,D>::Node(rootKey)){}
+
+    //destructor - when removing template class , first destruct the template and then the node
+    virtual~ AVLTree();
+
+    Node<K,D>* rightRotation(Node<K,D>* node);
+    Node<K,D>* leftRotation(Node<K,D>* node);
+    Node<K,D>* LRRotation(Node<K,D>* node);
+    Node<K,D>* RLRotation(Node<K,D>* node);
+
+    Node<K,D>* balance(Node<K,D>* unbalancedNode);
+    Node<K,D>* findKey(int key, Node<K,D>* curRoot) const;
+    Node<K,D>* insertNodeToTree(Node<K,D>* Newnode, Node<K,D>* currRoot);
+    Node<K,D>* DeleteNodeFromTree(Node<K,D>* currRoot, K key);
+
+    void preOrderPrint(Node<K,D>* root);
+};
+
+
+// Assume we got a valid input that isn't already in the tree - NEED TO CHECK OUTSIDE IN THE ADD FUNCTION!!
+template <typename K, typename D>
+Node<K,D>* AVLTree<K,D>::insertNodeToTree(Node<K,D>* Newnode, Node<K,D>* currRoot){
+    if(currRoot->getKey() < Newnode->getKey()){
+        if(currRoot->getRight() == nullptr){
+            currRoot->setRight(Newnode);
+            currRoot->setHeight(currRoot->getHeight());
+            //height = max(left->getHeight(), right->getHeight())+1;
+            return currRoot;
+        }
+        else{
+            currRoot->setRight(this->insertNodeToTree(Newnode, currRoot->getRight()));
+        }
+    }
+    if(currRoot->getKey() > Newnode->getKey()){
+        if (currRoot->getLeft() == nullptr){
+            currRoot->setLeft(Newnode);
+            currRoot->setHeight(currRoot->getHeight());
+            return currRoot;
+        }
+        else{
+            currRoot->setLeft(this->insertNodeToTree(Newnode, currRoot->getLeft()));
+        }
+    }
+    currRoot->setHeight(currRoot->getHeight());
+    int balanceFactor = currRoot->getBalanceFactor();
+    if (balanceFactor == -2 || balanceFactor == 2){
+        return balance(currRoot);
+    }
+    return currRoot;
+}
+
+template <typename K, typename D>
+Node<K,D>* AVLTree<K,D>::balance(Node<K,D>* unbalancedNode){
+    if(unbalancedNode->getBalanceFactor() == 2){
+        if(unbalancedNode->getLeft()->getBalanceFactor() >= 0){
+            return rightRotation(unbalancedNode);
+        }
+        else if (unbalancedNode->getLeft()->getBalanceFactor() == -1){
+            return LRRotation(unbalancedNode);
+        }
+    }
+    else if (unbalancedNode->getBalanceFactor() == -2){
+        if(unbalancedNode->getRight()->getBalanceFactor() <=0){
+            return leftRotation(unbalancedNode);
+        }
+        else if (unbalancedNode->getRight()->getBalanceFactor() == 1){
+            return RLRotation(unbalancedNode);;
+        }
+    }
+    return unbalancedNode;
+}
+
+//find key in AVL tree and return a pointer to it
+template <typename K, typename D>
+Node<K,D>* AVLTree<K,D>::findKey(int key, Node<K,D>* curRoot) const{
+    if (key == curRoot->getKey()){
+        return curRoot;
+    }
+    else if (key < curRoot->getKey() && curRoot->getLeft() != nullptr){
+        return findKey(key, curRoot->getLeft());
+    }
+    else if (key > curRoot->getKey() && curRoot->getRight() != nullptr){
+        return findKey(key, curRoot->getRight());
+    }
+    return nullptr;
+}
+
+template <typename K, typename D>
+Node<K,D>* AVLTree<K,D>::DeleteNodeFromTree(Node<K,D>* currRoot, K key) {
+    if (currRoot == nullptr) {
+        return nullptr;
+    }
+    if (key < currRoot->getKey()) {
+        currRoot->setLeft(this->DeleteNodeFromTree(currRoot->getLeft(), key));
+    }
+    else if (key > currRoot->getKey()) {
+        currRoot->setRight(this->DeleteNodeFromTree(currRoot->getRight(), key));
+    }
+    if (key == currRoot->getKey()){ //find the node to delete
+        if (currRoot->getLeft() == nullptr && currRoot->getRight() == nullptr) {//is leaf
+            delete currRoot;
+            return nullptr;
+
+        }
+        else if (currRoot->getLeft() == nullptr) {// have one child
+            Node<K,D>* temp = new Node<K,D>(currRoot->getRight());
+            delete currRoot;
+            return temp;
+        }
+        else if (currRoot->getRight() == nullptr) {// have one child
+            Node<K,D>* temp = new Node<K,D>(currRoot->left);
+            delete currRoot;
+            return temp;
+        }
+        else {//have two child
+            Node<K,D>* temp = currRoot->getRight();
+            while (temp->getLeft() != nullptr) {
+                temp = temp->getLeft();
+            }
+            currRoot->getKey() = temp->getKey();
+            currRoot->setData(temp->getData());
+            currRoot->setRight(this->DeleteNodeFromTree(currRoot->getRight(), temp->getKey()));
+        }
+    }
+    // Update height of the current node
+    currRoot->setHeight(currRoot->getHeight());
+    if (currRoot->getBalanceFactor() == 2 || currRoot->getBalanceFactor() == -2){
+        return balance(currRoot);
+    }
+    return currRoot;
+}
+
+template <typename K, typename D>
+//LL rotation - this is Tree, node is unbalanced node. right rotation and updates the heights, and returns the new root
+Node<K,D>* AVLTree<K,D>::rightRotation(Node<K,D>* node){
+    Node<K,D>* x = node->getLeft();
+    node->setLeft(x->getRight());
+    x->setRight(node);
+    node->setHeight(node->getHeight());
+    x->setHeight(x->getHeight());
+    return x;
+}
+
+template <typename K, typename D>
+//RR rotation - left rotation, updates the heights, and returns the new root
+Node<K,D>* AVLTree<K,D>::leftRotation(Node<K,D>* node){
+    Node<K,D>* x = node->getRight();
+    node->setRight(x->getLeft());
+    x->setLeft(node);
+    node->setHeight(node->getHeight());
+    x->setHeight(x->getHeight());;
+    return x;
+}
+
+template <typename K, typename D>
+Node<K,D>* AVLTree<K,D>::LRRotation(Node<K,D>* node){
+    node->setLeft(leftRotation(node->getLeft()));
+    return rightRotation(node);
+}
+
+//RR rotation - rotates, updates the heights, and returns the new root
+template <typename K, typename D>
+Node<K,D>* AVLTree<K,D>::RLRotation(Node<K,D>* node){
+    node->setRight(rightRotation(node->getRight()));
+    return leftRotation(node);
+}
+
+
+template <typename K, typename D>
+void AVLTree<K,D>::preOrderPrint(Node<K,D>* root){
+    if(this != nullptr)
+    {
+        std::cout << root->id << " ";
+        this->preOrderPrint(root->getLeft());
+        this->preOrderPrint(root->getRight());
+    }
+}
+
+#endif //MIVNAI_WET1_NEW_AVLTREE_H
---
+++ Node.h
@@ -0,0 +1,160 @@
+//
+// Created by Sapir on 20/02/2024.
+//
+
+#ifndef MIVNAI_WET1_NEW_NODE_H
+#define MIVNAI_WET1_NEW_NODE_H
+#include <iostream>
+#include "wet1util.h"
+int max (int a, int b);
+
+template <typename K, typename D>
+class Node {
+private:
+    K key ;
+    D *data;
+    Node *left;
+    Node *right;
+    int height;
+
+public:
+//constructor
+    explicit Node(K key): key(key), data(nullptr), left(nullptr), right(nullptr), height(0) {}
+    Node(K key, D* data): key(key), data(data), left(nullptr), right(nullptr), height(0) {}
+
+    //copy constractor
+    Node(Node<K, D>* nodePtr): key(nodePtr->key), data(nodePtr->data), left(nodePtr->left), right(nodePtr->right), height(nodePtr->height) {}
+//destructor - when removing template class , first destruct the template and then the node
+    virtual~ Node(){
+        delete data;
+        data = nullptr;
+        delete left;
+        left = nullptr;
+        delete right;
+        right = nullptr;
+    }
+
+    bool isSameKey (const Node other) const ;
+
+    int getHeight () const ;
+    int getBalanceFactor() const;
+    Node<K,D>* getRight() const;
+    Node<K,D>* getLeft() const;
+    D* getData() const;
+    K getKey() const;
+    void firstSetData (D* newDataPtr);
+    void setRight(Node<K,D>* newRight);
+    void setLeft(Node<K,D>* newLeft);
+    void setHeight(int newHeight);
+    void setData(D* newData);
+};
+template <typename K, typename D>
+void Node<K,D>::setData(D* newData){
+    this->data = newData;
+}
+template <typename K, typename D>
+void Node<K,D>::setHeight(int newHeight){
+    this->height = newHeight;
+}
+template <typename K, typename D>
+void Node<K,D>::setRight(Node<K,D>* newRight){
+    this->right = newRight;
+}
+template <typename K, typename D>
+void Node<K,D>::setLeft(Node<K,D>* newLeft){
+    this->left = newLeft;
+}
+template <typename K, typename D>
+int max (int a, int b) {
+    return (a>b) ? a : b;
+}
+//maybe delete it
+template <typename K, typename D>
+bool Node<K,D>::isSameKey (const Node other) const {
+    if (other.key == key) {
+        return true;
+    }
+    return false;
+}
+//changedddddddddddddddddddd
+template<typename K, typename D>
+int Node<K,D>::getHeight() const {
+    if(this->left == nullptr && this->right == nullptr){
+        return 0;
+    }
+    return 1 + max((this->left->height , this->right->height));
+}
+
+template<typename K, typename D>
+Node<K,D>* Node<K,D>::getRight() const {
+    return right;
+}
+
+template<typename K, typename D>
+Node<K,D>* Node<K,D>::getLeft() const {
+    return left;
+}
+
+template<typename K, typename D>
+D* Node<K,D>::getData() const {
+    return data;
+}
+
+template<typename K, typename D>
+K Node<K,D>::getKey() const{
+    return key;
+}
+
+template<typename K, typename D>
+void Node<K,D>::firstSetData (D* newDataPtr){
+    data=newDataPtr;
+}
+
+template<typename K, typename D>
+int Node<K,D>::getBalanceFactor() const{
+    if (this->left != nullptr && this->right != nullptr) {
+        return (this->left->getHeight() - this->right->getHeight());
+    }
+    if (this->left != nullptr && this->right == nullptr) {
+        return (this->left->getHeight());
+    }
+    if (this->left == nullptr && this->right != nullptr) {
+        return ( this->right->getHeight());
+    }
+    return 0;
+}
+
+
+/*
+int main(){
+
+Node<int>* root = new Node<int>(21);
+
+Node<int>* a = new Node<int>(23);
+Node<int>* b = new Node<int>(22);
+Node<int>* c = new Node<int>(19);
+root = root->insertNode(a);
+root = root->insertNode(b);
+root = root->insertNode(c);
+root = root->deleteNode(root,23);
+root->preOrderPrint();
+
+Node<int>* a = new Node<int>(23);
+Node<int>* b = new Node<int>(22);
+Node<int>* c = new Node<int>(19);
+//  root->preOrderPrint();
+
+//root->preOrderPrint();
+root = root->deleteNode(root,23);
+// root->preOrderPrint();
+// root = root->deleteNode(root,7);
+root = root->deleteNode(root,21);
+root->preOrderPrint();
+    return 0;
+    }
+*/
+int main(){
+
+    return 0;
+}
+#endif //MIVNAI_WET1_NEW_NODE_H
