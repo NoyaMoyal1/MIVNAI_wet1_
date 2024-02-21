789648777e042eace4f90b9fccdba7ad2f7d8f42
sapirfishman <sapirfishman@campus.technion.ac.il>
2024-02-21 23:49:34 +0200

    function in olimpic except team func are done

--- Olympicsa1.cpp
+++ Olympicsa1.cpp
@@ -108,6 +108,59 @@ StatusType Olympics::update_contestant_strength(int contestantId ,int change){
     if (contestantId <= 0){
         return StatusType::INVALID_INPUT;
     }
+    if (m_contestantTree->findKey(contestantId,m_contestantTree->getRoot()) == nullptr){
+        return StatusType::FAILURE;
+    }
+    Node<int,Contestant>* currContestant = m_contestantTree->findKey(contestantId, m_contestantTree->getRoot());
+    int oldStrength = currContestant->getData()->get_strength();
+    int newStrength = oldStrength + change;
+    if (newStrength < 0){
+        return StatusType::FAILURE;
+    }
+    currContestant->getData()->set_strength(newStrength);
+    for (int i = 0; i < currContestant->getData()->get_numTeam() ; ++i) {
+        Node<int, Team> *teamNode = m_TeamTree->findKey(currContestant->getData()->get_teamsArr()[i],
+                                                        m_TeamTree->getRoot());
+
+        StrengthPairKey oldKey = StrengthPairKey(oldStrength, currContestant->getKey());
+        StrengthPairKey newKey = StrengthPairKey(newStrength, currContestant->getKey());
+
+        Node<StrengthPairKey, StrengthInfo> *currRootLeft = teamNode->getData()->getRightTreeStrength()->getRoot();
+        Node<StrengthPairKey, StrengthInfo> *currRootRight = teamNode->getData()->getRightTreeStrength()->getRoot();
+        Node<StrengthPairKey, StrengthInfo> *currRootMiddle = teamNode->getData()->getMiddleTreeStrength()->getRoot();
+
+        Node<StrengthPairKey, StrengthInfo> *oldStrengthNodeLeft = teamNode->getData()->getLeftTreeStrength()->findKey(
+                oldKey,
+                teamNode->getData()->getLeftTreeStrength()->getRoot());
+        Node<StrengthPairKey, StrengthInfo> *oldStrengthNodeRight = teamNode->getData()->getRightTreeStrength()->findKey(
+                oldKey,
+                teamNode->getData()->getLeftTreeStrength()->getRoot());
+        Node<StrengthPairKey, StrengthInfo> *oldStrengthNodeMiddle = teamNode->getData()->getMiddleTreeStrength()->findKey(
+                oldKey,
+                teamNode->getData()->getLeftTreeStrength()->getRoot());
+
+        Node<StrengthPairKey, StrengthInfo> *newStrengthNode = new Node<StrengthPairKey, StrengthInfo>(newKey,
+                                                                                                       currContestant->getData()->getStrengthInfo());
+        //looking for the strength in left tree
+        if (oldStrengthNodeLeft != nullptr) {
+            teamNode->getData()->getLeftTreeStrength()->DeleteNodeFromTree(currRootLeft, oldKey);
+            teamNode->getData()->getLeftTreeStrength()->insertNodeToTree(newStrengthNode, currRootLeft);
+        }
+            //looking for the strength in right tree
+        else if (oldStrengthNodeRight != nullptr) {
+            teamNode->getData()->getRightTreeStrength()->DeleteNodeFromTree(currRootRight, oldKey);
+            teamNode->getData()->getRightTreeStrength()->insertNodeToTree(newStrengthNode, currRootRight);
+        }
+            //looking for the strength in middle tree
+        else if (oldStrengthNodeMiddle != nullptr) {
+            teamNode->getData()->getMiddleTreeStrength()->DeleteNodeFromTree(currRootMiddle, oldKey);
+            teamNode->getData()->getMiddleTreeStrength()->insertNodeToTree(newStrengthNode, currRootMiddle);
+        }
+        //update team strength
+        teamNode->getData()->setTeamStrength(teamNode->getData()->calcStrength());
+    }
+
+    return StatusType::SUCCESS;
 }
 
 output_t<int> Olympics::get_strength(int contestantId){
@@ -150,7 +203,7 @@ output_t<int> Olympics::austerity_measures(int teamId){
     if(m_TeamTree->findKey(teamId,m_TeamTree->getRoot()) == nullptr){
         return StatusType::FAILURE;
     }
-    if(this->m_TeamTree->findKey(teamId,m_TeamTree->getRoot())->getData()->getNumOfContestant < 3){
+    if(  m_TeamTree->findKey(teamId,m_TeamTree->getRoot())->getData()->getNumOfContestant() < 3){
         return StatusType::FAILURE;
     }
 
